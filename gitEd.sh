89e9a75c18869871db667e60b7d5cc5a4e5e6422
mauricio duque orozco <mauricio.duque.eje@icloud.com>
2015-08-19 19:49:15 -0500

    28

--- gitEd.sh
+++ gitEd.sh
@@ -1,7 +1,7 @@
 echo $1
-echo $2
+#echo $2
 git rm $(git ls-files --deleted)
 git add .
-git tag -a $1 
-git commit -m $2
+#git tag -a $1 
+git commit -m $1
 git push -u git@github.com:mauricioduqueorozco/projectLab.git
